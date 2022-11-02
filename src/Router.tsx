b25dfe5380e741ae442b73fbb29098b0eedde6f2
naoki.tomita <naoki.tomita@uzabase.com>
2022-11-02 22:45:12 +0900

    Use zstyl

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,4 +1,5 @@
 import { h, Component } from "zheleznaya";
+import { styled } from "zstyl";
 
 type RouteCallback = (route: string) => void;
 const cbs: RouteCallback[] = [];
@@ -13,10 +14,15 @@ function emit() {
 
 window.addEventListener("hashchange", _ => emit());
 
+const A = styled.a`
+  color: inherit;
+  text-decoration: none;
+`;
+
 export const Link: Component = ({ href }: { href: string }, children) => {
   return (
-    <a style={{ color: "inherit", textDecoration: "none" }} href={`#${href}`}>
+    <A href={`#${href}`}>
       {children}
-    </a>
+    </A>
   );
 }
