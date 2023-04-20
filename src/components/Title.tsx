8428f04a171e14ef50943e971808cae53e1df2a6
Naoki Tomita🐱 <mxcpy608@hotmail.com>
2023-04-20 22:25:49 +0900

    Improve null check.

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,5 +1,5 @@
 import { h, Component } from "zheleznaya";
-let title: HTMLTitleElement = document.querySelector("title")!;
+let title: HTMLTitleElement = globalThis?.document?.querySelector("title")!;
 
 export const Title: Component<{ label: string }> = ({ label }) => {
   title.innerText = label;
