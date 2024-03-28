a954badde5916a5655d115d74059c0a81da25422
Naoki Tomita🐱 <mxcpy608@hotmail.com>
2024-03-28 12:09:02 +0900

    Add sentry

--- index.tsx
+++ index.tsx
@@ -1,4 +1,20 @@
 import { render, h } from "zheleznaya";
 import { App } from "./src/components/App";
+import * as Sentry from "@sentry/browser";
+
+Sentry.init({
+  dsn: "https://c57ad2b04d85f44ff4941c4cba19f869@o4506986090266624.ingest.us.sentry.io/4506986094723072",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration({
+      maskAllText: false,
+      blockAllMedia: false,
+    }),
+  ],
+  tracesSampleRate: 1.0,
+  tracePropagationTargets: ["localhost", "zheleznaya.netlify.app"],
+  replaysSessionSampleRate: 1.0,
+  replaysOnErrorSampleRate: 1.0,
+});
 
 render(<App />);
--- src/Store.ts
+++ src/Store.ts
@@ -1,7 +1,7 @@
 import marked from "marked";
 import { createStore } from "zheleznaya";
 import { highlightAll } from "./Prism";
-import { onRouteChange } from "./Router";
+import { HashStrategy, createRouter } from "@kojiro.ueda/zrouter";
 
 export const store = createStore<{
   path: string;
@@ -14,15 +14,12 @@ export const store = createStore<{
   html: "",
   loading: false,
 });
-onRouteChange((path) => href(path));
+const router = createRouter(store, HashStrategy);
+router.onRouteChange(() => href(store.path.replace("#", "")));
 
 loadMakdown(`${store.path}.md`);
 
 export function href(path: string) {
-  if (path === store.path) {
-    return;
-  }
-  store.path = path;
   loadMakdown(`${path}.md`);
 }
 
@@ -35,17 +32,13 @@ async function markedAsync(md: string): Promise<string> {
 }
 
 export async function loadMakdown(path: string): Promise<void> {
-  if (path === "/.md") {
-    location.hash = "#index";
-    return;
-  }
   store.loading = true;
   try {
     const markdown = await fetch(`../articles/${path}`)
       .then(it => it.ok ? it.text() : "")
       .then(it => it.trim());
     if (!markdown) {
-      location.hash = "#index";
+      router.replace("index");
       return;
     }
     const html = document.createElement("div");
